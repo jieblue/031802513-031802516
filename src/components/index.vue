9f5debf25697357a5f5d9fa5b8bbe2c8e508addc
jieblue <53696774+jieblue@users.noreply.github.com>
2020-10-07 23:34:50 +0800

    引入vue-tree-chart,fix little bug

--- src/components/index.vue
+++ src/components/index.vue
@@ -2,29 +2,32 @@
   <div class="index">
    <el-row :gutter="20"  class="el-row" type="flex" >
     <el-col :span="8" :offset="8">
-     <el-input
+     <el-input :rows="8"
   type="textarea"
-  autosize
   placeholder="请输入内容"
   v-model="text">
+  <!-- v-show="show" -->
+  
 </el-input>
     </el-col>
    </el-row>
       <el-row :gutter="20"  class="el-row" type="flex" >
    <el-col :span="8" :offset="8">
  
-   <el-button @click="init">生成</el-button>
+   <el-button @click="init" >生成</el-button>
      </el-col>
     </el-row>
-    <tree  v-for="item in teacher" :key="item.name" :teacherdata="item" >aaa</tree>
+        <div v-drag class="card">
+    <TreeChart :json="item"  v-for="item in teacher" :key="item.name"   ></TreeChart>
+    </div>
   </div>
 </template>
 
 <script>
-import tree from './tree'
+import TreeChart from './tree'
 
 export default {
-  components: {tree},
+  components: {TreeChart},
   name: 'index',
   data () {
     return {
@@ -32,37 +35,39 @@ export default {
       lines:"",
       teacher:[],
       students:[],
-      msg:""
+      show:true,
     }
   },
   methods:{
     
     init(){
+      this.teacher=[];
+      this.students=[];
       this.initData();
-      this.msg="haha"
+     this.show=false;
       console.log(this.teacher)
     },
-    initData(){
+      initData(){
       var nowTeacher="";
       this.lines=this.text.split('\n');
       this.lines.forEach(item=>{
       if(item.includes("导师：")){
         var temp=item.split("：")
         nowTeacher=temp[1];
-         this.teacher.push({label:temp[1],children:[]})
+         this.teacher.push({name:temp[1],children:[]})
       }else if(item===""){
         nowTeacher=""
       }else if(nowTeacher!==""){
         var temp=item.split("：");
         var students=temp[1].split("、");
         this.teacher.filter(item => {
-      if (item.label===nowTeacher) {
+      if (item.name===nowTeacher) {
         var children=[];
         students.forEach(s => {
-          children.push({label:s});
+          children.push({name:s});
           
         });
-          item.children.push({label:temp[0],children:children});
+          item.children.push({name:temp[0],children:children});
       }
       })
        
@@ -81,7 +86,35 @@ export default {
     }
 
 
-  }
+  },
+     directives: {
+        drag: {
+            // 指令的定义
+            bind: function(el) {
+                let oDiv = el;  // 获取当前元素
+                oDiv.onmousedown = (e) => {
+                    // 算出鼠标相对元素的位置
+                    let disX = e.clientX - oDiv.offsetLeft;
+                    let disY = e.clientY - oDiv.offsetTop;
+
+                    document.onmousemove = (e) => {
+                        // 用鼠标的位置减去鼠标相对元素的位置，得到元素的位置
+                        let left = e.clientX - disX;
+                        let top = e.clientY - disY;
+
+                        oDiv.style.left = left + 'px';
+                        oDiv.style.top = top + 'px';
+                    };
+
+                    document.onmouseup = (e) => {
+                        document.onmousemove = null;
+                        document.onmouseup = null;
+                    }
+                }
+            }
+        }
+    }
+
 }
 </script>
 
@@ -101,4 +134,11 @@ li {
 a {
   color: #42b983;
 }
+    .card {
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+
+    }
 </style>
